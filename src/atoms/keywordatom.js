843bf87a44ecef686bbd97aaed0c0d548f8cd849
Black-Jade0 <160476867+Black-Jade0@users.noreply.github.com>
2024-12-15 10:43:37 +0530

    chatbot

---
+++ src/atoms/keywordatom.js
@@ -0,0 +1,10 @@
+import { atom } from "recoil";
+
+export const keywordatom = atom({
+    key: "keywordatom",
+    default: "",
+});
+export const ChatBotState = atom({
+    key: "chatstate",
+    default: false, // Set default to false (boolean)
+});
